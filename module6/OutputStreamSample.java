e0478cebea32fe0612db218b1660b6904e990f3a
Harris Rainier <ohrainier@gmail.com>
2022-04-21 09:00:51 +0800

    Uploaded more examples

---
+++ module6/OutputStreamSample.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class OutputStreamSample {
+
+	public static void main(String[] args) {
+		String s = "ABCDF";
+		byte[] data = s.getBytes();
+		OutputStream output = null;
+		try {
+			output = System.out;
+			output.write(data);
+			output.flush();
+			output.close();
+		} catch (Exception e) {
+			System.out.println("Message: " + e);
+		}
+
+	}
+
+}
